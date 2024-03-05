730cc79830313e0964ddf2d37548267c9fccc4f9
justyna <towarnickaj@gmail.com>
2024-03-05 13:46:53 +0100

    clean up

--- src/types/staff-scraper/staff.type.ts
+++ src/types/staff-scraper/staff.type.ts
@@ -1,12 +1,11 @@
 export interface AcademicContent {
-    email: string;
-    posts: { position: string; faculty: string[] }[];
-    tutorial: string;
+  email: string;
+  posts: { position: string; faculty: string[] }[];
+  tutorial: string;
 }
 
 export interface Academic {
-    name: string;
-    link: string;
-    units: string[];
-    content: AcademicContent;
+  name: string;
+  link: string;
+  content: AcademicContent;
 }
