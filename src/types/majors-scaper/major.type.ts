8823565f10f31de3dc64ba7d119da1e859290249
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2024-03-05 20:13:22 +0100

    fix majors scraper

--- src/types/majors-scaper/major.type.ts
+++ src/types/majors-scaper/major.type.ts
@@ -1,18 +1,8 @@
 import { Degree } from './degree.type';
 
 export interface Major {
-    name: {
-        PL: string;
-        EN?: string;
-    };
-    url: string;
-    content: { PL: string | null; EN?: string | null };
-    degree: Degree;
-}
-
-export interface MajorOutput {
-    name: string;
-    url: string;
-    content: string;
-    degree: Degree;
+  name: string;
+  url: string;
+  content: string | null;
+  degree: Degree;
 }
