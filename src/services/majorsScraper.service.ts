ed539caab6008d69abdac6269f73349124e9fca5
jzapiorkowski <92112490+jzapiorkowski@users.noreply.github.com>
2024-03-12 17:35:36 +0100

    feat: udpate majors scraper (#1)
    * fix majors scraper
    
    * add ts-node
    
    * add ts-jest

--- src/services/majorsScraper.service.ts
+++ src/services/majorsScraper.service.ts
@@ -7,130 +7,125 @@ import { removeNewLines } from '../utils/removeNewLines';
 import { Degree } from '../types/majors-scaper/degree.type';
 
 export interface ErrorType {
-    status: number;
-    message: string;
+  status: number;
+  message: string;
 }
 
 const prepareITURLs = async (
-    degree: Degree,
+  degree: Degree
 ): Promise<Partial<Major>[] | null[]> => {
-    try {
-        const degreeURL =
-            degree === Degree.BACHELOR
-                ? 'studia-i-stopnia'
-                : 'studia-ii-stopnia';
+  try {
+    const degreeURL =
+      degree === Degree.BACHELOR ? 'studia-i-stopnia' : 'studia-ii-stopnia';
 
-        const url = `https://mfi.ug.edu.pl/rekrutacja/${degreeURL}/informatyka/tryb-stacjonarny`;
+    const url = `https://mfi.ug.edu.pl/rekrutacja/${degreeURL}/informatyka/tryb-stacjonarny`;
 
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
 
-        const ITMajorsURLs = $('#block-ug-mfi-theme-menu-glowne > ul a')
-            .map((index, element): Partial<Major> | Partial<Major>[] | null => {
-                const majorEndpoint = $(element).attr('href');
-                const majorName = $(element).text();
+    const ITMajorsURLs = $(
+      '#views-bootstrap-prezentacja-w-kategorii-terminy-blok-poddzialy > div a'
+    )
+      .map((index, element): Partial<Major> | Partial<Major>[] | null => {
+        const majorEndpoint = $(element).attr('href');
+        const majorName = $(element).text();
 
-                if (majorName === 'Tryb stacjonarny') return null;
+        if (majorName === 'Tryb stacjonarny') return null;
 
-                return {
-                    name: { PL: fixMajorName(majorName) },
-                    url: 'https://mfi.ug.edu.pl' + majorEndpoint,
-                };
-            })
-            .get();
+        return {
+          name: fixMajorName(majorName),
+          url: 'https://mfi.ug.edu.pl' + majorEndpoint,
+        };
+      })
+      .get();
 
-        return ITMajorsURLs;
-    } catch (error) {
-        return [null];
-    }
+    return ITMajorsURLs;
+  } catch (error) {
+    return [null];
+  }
 };
 
 const majorScraper = async (
-    partialMajor: Partial<Major>,
-    degree: Degree,
+  partialMajor: Partial<Major>,
+  degree: Degree
 ): Promise<Major | null> => {
-    try {
-        if (!partialMajor.url) return null;
+  try {
+    if (!partialMajor.url) return null;
 
-        const { data } = await axios.get(partialMajor.url);
-        const $ = cheerio.load(data);
+    const { data } = await axios.get(partialMajor.url);
+    const $ = cheerio.load(data);
 
-        const majorBody = $(
-            '.taxonomy-term > div > div > div > div > div > div > span > article > div.node__content.clearfix > div',
-        ).html();
+    const majorBody = $(
+      '.taxonomy-term > div > div > div > div > div > div > span > article > div.node__content.clearfix > div'
+    ).html();
 
-        return {
-            url: partialMajor.url,
-            content: {
-                PL: majorBody ? removeNewLines(majorBody) : null,
-                EN: null,
-            },
-            name: { PL: partialMajor.name!.PL || '', EN: '' },
-            degree,
-        };
-    } catch (error) {
-        return null;
-    }
+    return {
+      url: partialMajor.url,
+      content: majorBody ? removeNewLines(majorBody) : null,
+      name: partialMajor.name!,
+      degree,
+    };
+  } catch (error) {
+    return null;
+  }
 };
 
 const majorsScraperByDegree = async (degree: Degree): Promise<Major[]> => {
-    const degreeURL =
-        degree === Degree.BACHELOR ? 'studia-i-stopnia' : 'studia-ii-stopnia';
+  const degreeURL =
+    degree === Degree.BACHELOR ? 'studia-i-stopnia' : 'studia-ii-stopnia';
 
-    const { data } = await axios.get(
-        `https://mfi.ug.edu.pl/rekrutacja/${degreeURL}`,
-    );
+  const { data } = await axios.get(
+    `https://mfi.ug.edu.pl/rekrutacja/${degreeURL}`
+  );
 
-    const $ = cheerio.load(data);
+  const $ = cheerio.load(data);
 
-    const majorsNavElementPath =
-        '#block-ug-mfi-theme-menu-glowne > ul > li.nav-item.menu-item--expanded.menu-item--active-trail > ul > li';
+  const majorsNavElementPath =
+    '#collapseExample > ul > li.nav-item.menu-item--expanded.menu-item--active-trail > ul li';
 
-    const majors = await Promise.all(
-        $(majorsNavElementPath)
-            .map(
-                async (
-                    index,
-                    element,
-                ): Promise<
-                    | Partial<Major>
-                    | Partial<Major>[]
-                    | (Partial<Major> | null)[]
-                > => {
-                    const majorEndpoint = $(element).find('a').attr('href');
-                    const majorName = $(element).find('a').text();
+  const majors = await Promise.all(
+    $(majorsNavElementPath)
+      .map(
+        async (
+          index,
+          element
+        ): Promise<
+          Partial<Major> | Partial<Major>[] | (Partial<Major> | null)[]
+        > => {
+          const majorEndpoint = $(element).find('a').attr('href');
+          const majorName = $(element).find('a').text();
 
-                    if (majorName === 'Informatyka') {
-                        return await prepareITURLs(degree);
-                    }
+          if (majorName === 'Informatyka') {
+            return await prepareITURLs(degree);
+          }
 
-                    return {
-                        name: { PL: majorName },
-                        url: 'https://mfi.ug.edu.pl' + majorEndpoint,
-                    };
-                },
-            )
-            .get(),
-    );
+          return {
+            name: majorName,
+            url: 'https://mfi.ug.edu.pl' + majorEndpoint,
+          };
+        }
+      )
+      .get()
+  );
 
-    const majorsFullInfo = await Promise.all(
-        majors
-            .flat()
-            .filter((major): major is Major => major !== null)
-            .map((major) => majorScraper(major, degree)),
-    );
+  const majorsFullInfo = await Promise.all(
+    majors
+      .flat()
+      .filter((major): major is Major => major !== null)
+      .map((major) => majorScraper(major, degree))
+  );
 
-    return majorsFullInfo.filter((major): major is Major => major !== null);
+  return majorsFullInfo.filter((major): major is Major => major !== null);
 };
 
 export const majorsInfoScraper = async (): Promise<Major[] | ErrorType> => {
-    try {
-        const majorsBachelor = majorsScraperByDegree(Degree.BACHELOR);
-        const majorsMaster = majorsScraperByDegree(Degree.MASTER);
-        return Promise.all([majorsBachelor, majorsMaster]).then((majors) =>
-            majors.flat(),
-        );
-    } catch (error) {
-        return returnScraperError(error);
-    }
+  try {
+    const majorsBachelor = majorsScraperByDegree(Degree.BACHELOR);
+    const majorsMaster = majorsScraperByDegree(Degree.MASTER);
+    return Promise.all([majorsBachelor, majorsMaster]).then((majors) =>
+      majors.flat()
+    );
+  } catch (error) {
+    return returnScraperError(error);
+  }
 };
