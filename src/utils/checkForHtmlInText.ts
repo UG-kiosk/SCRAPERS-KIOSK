6e9c925c4c6161c4418bd7fed0f4e5fafe429898
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2024-03-27 20:44:16 +0100

    update 2

--- src/utils/checkForHtmlInText.ts
+++
@@ -1,6 +0,0 @@
-export const checkForHtmlInText = (text: string): boolean => {
-    // TODO  add <a> tag along with elements with style=
-    const htmlElements = ['<li>'];
-
-    return htmlElements.some((element) => text.includes(element));
-};
